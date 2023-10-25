2566da8d806fbf9ecd3c087d2cb213068bfb5122
kriakemor <vjoelsuxod@gmail.com>
2023-10-25 13:20:09 -0400

    CRUD nos o

--- app/Http/Controllers/AdminPController.php
+++ app/Http/Controllers/AdminPController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Nosotros;
+use Illuminate\Support\Facades\Storage;
 
 class AdminPController extends Controller
 {
-    //
+    public function crearP(Request $request){
+        $imagen = $request->file('urlmagen')->store('public/Nosotros');
+        $urlFondoPath = Storage::url($imagen);
+
+    }
 }
